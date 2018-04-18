0c2fbf5771b28ba1740011c32435fc056245c877
Mike Eling <mike.eling97@gmail.com>
2018-04-18 20:54:22 +0200

    Revert "Revert "Initial structure""
    This reverts commit af19bee0aa901c0677e3405aa4bb243b2157a7cb.

---
+++ src/components/header/index.js
@@ -0,0 +1,18 @@
+import React { Component } from 'react';
+
+class Header extend Component {
+	constructor(props) {
+	super(props);	
+		this.state = {
+
+		}
+	}
+
+	render() {
+		return() {
+			
+		}
+	}
+}
+
+export default {Header };
